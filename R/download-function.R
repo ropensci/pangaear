bcefce19a7efc7aedf40fac54a5fc4a2b2078b7f
Gavin Simpson <ucfagls@gmail.com>
2014-09-03 17:42:06 -0600

    Revert "patch up download-function"
    This reverts commit e3e00406e36a88821ebf12d3f3920c029106c7b0.

--- R/download-function.R
+++ R/download-function.R
@@ -1,8 +1,19 @@
 #' Function to download data from Pangaea
 #'
-#' This function allows you to download a zip file of data from the Pangaea data repository \url{http://www.pangaea.de}. Grabs data as dataframe from a URI. Expects the response data to be in JSON format
+#' This function allows you to download a zip file of data from the Pangaea data repository http://www.pangaea.de/
+#' @import RCurl devtools
+#' @param
+#' @keywords
+#' @export
+#' @examples
+#' pangaea_download()
+
+library("devtools")
+library("RCurl")
+
+#' .get_data_from_uri
 #'
-#' @importFrom RCurl getURLContent
+#' Grabs data as dataframe from a URI. Expects the response data to be in JSON format
 #'
 #' @param uri Public internet address of the data
 #' @return dataframe with rows of data
@@ -10,14 +21,15 @@
 #' .get_data_from_uri("http://api.example.com/ecologydata")
 #' }
 
+# testing
+# uri <- "http://doi.pangaea.de/10.1594/PANGAEA.820036?format=textfile"
+# uri <- "http://doi.pangaea.de/10.1594/PANGAEA.807575?format=textfile"
+
 .get_data_from_uri<-function(uri){
-    response <- getURLContent(uri)
+
+    response <- RCurl::getURLContent(uri)
     raw_data <- gsub("/\\*.*\\*/\n", "", response)
     df <- read.delim(textConnection(raw_data))
     df
 
 }
-
-# testing
-# uri <- "http://doi.pangaea.de/10.1594/PANGAEA.820036?format=textfile"
-# uri <- "http://doi.pangaea.de/10.1594/PANGAEA.807575?format=textfile"
