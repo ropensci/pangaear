ec408aea89182cc5ad113267c5593eeae249f9bd
Andrew <a.a.m.macdonald@gmail.com>
2014-09-03 15:26:22 -0700

    fixed typo

--- R/pg_getrecord.R
+++ R/pg_getrecord.R
@@ -7,7 +7,7 @@
 #' @examples
 #' pg_getrecord()
 
-pg_getrecord(Identifier, transform = TRUE){
+pg_getrecord <- function(Identifier, transform = TRUE){
   baseurl <- "http://ws.pangaea.de/oai/"
   oaih_get_record(baseurl, identifier = Identifier, transform = transform)
 }
