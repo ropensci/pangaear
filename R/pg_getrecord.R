3c4cc1cd93172ff5a6496d65a323a587e4cbe164
Gavin Simpson <ucfagls@gmail.com>
2014-09-04 10:18:56 -0600

    rename pg_getrecord to pg_get_record

--- R/pg_getrecord.R
+++ R/pg_getrecord.R
@@ -6,11 +6,11 @@
 #' @param transform logical;
 #'
 #' @examples \dontrun{
-#' record <- pg_getrecord(identifier = "oai:pangaea.de:doi:10.1594/PANGAEA.788382")
+#' record <- pg_get_record(identifier = "oai:pangaea.de:doi:10.1594/PANGAEA.788382")
 #' head(record)
 #' }
 
-pg_getrecord <- function(Identifier, transform = TRUE){
+pg_get_record <- function(Identifier, transform = TRUE){
   baseurl <- "http://ws.pangaea.de/oai/"
   oaih_get_record(baseurl, identifier = identifier, transform = transform)
 }
