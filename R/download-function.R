39a10c18b4795ab3fd49cb2a34285bdf32d24ce2
Naupaka Zimmerman <naupaka@gmail.com>
2014-09-03 19:06:08 -0700

    changed function names to convention, added docs

--- R/download-function.R
+++ R/download-function.R
@@ -11,7 +11,7 @@
 #' .get_data_from_uri("http://api.example.com/ecologydata")
 #' }
 
-.get_data_from_uri <- function(uri){
+pg_get_data_from_uri <- function(uri){
     uri <- paste0(uri,"?format=textfile")
     response <- RCurl::getURLContent(uri)
     raw_data <- gsub("/\\*.*\\*/\n", "", response)
@@ -19,7 +19,16 @@
     df
 }
 
-.get_multiple_data_from_uri <- function(uri){
+#' @author Naupaka Zimmerman
+#' @import RCurl devtools
+#' @export
+#' @param uri Public internet address of the data
+#' @return dataframe with rows of data, list of dataframes with rows of data if there are multiple related data sets found at the URI
+#' @examples \donttest{
+#' .get_data_from_uri("http://api.example.com/ecologydata")
+#' }
+
+pg_get_multiple_data_from_uri <- function(uri){
 
   response <- RCurl::getURLContent(uri)
   if(grepl("name=\"dslist\"", response) == TRUE){
@@ -28,7 +37,7 @@
     data_file_dois <- unlist(data_file_dois)
     split_doi <- strsplit(data_file_dois, split = "\"")
     doi_vector <- sapply(split_doi, function (x) x[grepl("doi",x)])
-    out.temp <- lapply(doi_vector[1:2], .get_data_from_uri)
+    out.temp <- lapply(doi_vector[1:2], pg_get_data_from_uri)
   }
   else{
     raw_data <- gsub("/\\*.*\\*/\n", "", response)
