1c1c36b3da373265e962bd34cfe664b19239604e
Scott Chamberlain <myrmecocystus@gmail.com>
2014-10-27 14:49:59 -0700

    fixed package imports

--- R/pg_list_sets.R
+++ R/pg_list_sets.R
@@ -1,7 +1,6 @@
 #' List the set structure of the Pangaea repository
 #'
 #' @export
-#' @importFrom OAIHarvester oaih_list_sets
 #' @importFrom XML xmlParse
 #' @param ... Curl debugging options passed on to \code{\link[httr]{GET}}
 #'
@@ -15,12 +14,3 @@ pg_list_sets <- function(...) {
   res <- pg_GET(args = pgc(list(verb="ListSets")), ...)
   data.frame(do.call(rbind, res), stringsAsFactors = FALSE)
 }
-#   if (transform) {
-#     tmp <- oaih_list_sets(baseoai(), transform = transform)
-#     data.frame(tmp, stringsAsFactors = FALSE)
-#   } else {
-#     url2 <- paste(baseoai(), "/?verb=", "ListSets", sep = "")
-#     tmp <- GET(url2)
-#     tmp <- xmlParse(tmp)
-#   }
-#   tmp
