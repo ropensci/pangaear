84c15e81029016c398f21dd0e48a3131c0eeec46
Scott Chamberlain <myrmecocystus@gmail.com>
2014-10-27 14:46:46 -0700

    added list_identifiers fxn, closes #7

---
+++ R/pg_list_identifiers.R
@@ -0,0 +1,26 @@
+#' List identifiers of the Pangaea repository
+#'
+#' @export
+#' @importFrom httr GET content
+#' @inheritParams pg_list_records
+#'
+#' @examples \donttest{
+#' pg_list_identifiers(from='2012-01-01', until='2012-01-05')
+#' pg_list_identifiers(from='2013-10-01', until='2013-10-05')
+#' pg_list_identifiers(from='2013-10-01', until='2013-10-03', set="geocode1")
+#' pg_list_identifiers(prefix="iso19139", from='2012-01-01', until='2012-01-05')
+#' pg_list_identifiers(prefix="dif", from='2012-01-01', until='2012-01-05')
+#'
+#' library('httr')
+#' pg_list_identifiers(prefix="dif", from='2012-01-01', until='2012-01-05', config=verbose())
+#' }
+
+pg_list_identifiers <- function(prefix = "oai_dc", from = NULL, until = NULL, set = NULL, ...)
+{
+  args <- pgc(list(verb="ListIdentifiers", metadataPrefix=prefix, from=from, until=until, set=set))
+  res <- pg_GET(args = args, ...)
+  done <- lapply(res, function(x){
+    c(x[1:2], setSpec=paste0(unlist(x[names(x) %in% "setSpec"]), collapse = ","))
+  })
+  data.frame(do.call(rbind, done), stringsAsFactors = FALSE)
+}
